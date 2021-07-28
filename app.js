8c298ab4a0b695dfb7b9dae95e039c783c59e3c1
JMiller686 <jmiller686@gmail.com>
2021-07-28 17:17:50 -0500

    added and connected to mongodb, setup mongoose schema

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 
 //express app
 const app = express();
 
+//connect to mongo db
+const dbURI = 'mongodb+srv://mnmdev:WCgenVvx4w1cMifo@nodeblog.caikp.mongodb.net/mnmBlog?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
 //register view engine
 app.set('view engine', 'ejs');
 
 //listen for requests
-app.listen(3000);
+
+
+//middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.use((req,res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
 
 app.get('/', (req,res) => {
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem Ipsum dolor sit amet consectetur'},
-        { title: 'Mario finds stars', snippet: 'Lorem Ipsum dolor sit amet consectetur'},
-        { title: 'How to defeat bowser', snippet: 'Lorem Ipsum dolor sit amet consectetur'}
-    ]
-    res.render('index', {
-        title: 'Home',
-        blogs
-    });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req,res) => {
@@ -27,6 +39,15 @@ app.get('/about', (req,res) => {
         title: 'About'
     })
 });
+
+//blog routes
+app.get('/blogs', (req,res) => {
+    Blog.find().sort({ createdAt: -1})
+    .then(result => {
+        res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch(err => console.log(err));
+})
 
 app.get('/blogs/create', (req,res) => {
     res.render('create', {
